6e102ccd7b1be77ebaedc801bbd35bca1a5cbf35
Serghei Iakovlev <egrep@protonmail.ch>
2021-07-09 00:49:49 +0300

    Setting build type to 'Release' as none was specified

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -24,7 +24,7 @@ if(NOT isMultiConfig) # Makefiles, Ninja, ...
   # a known build type.
   if(NOT CMAKE_BUILD_TYPE)
     message(STATUS "Setting build type to 'Release' as none was specified.")
-    set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
+    set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build." FORCE)
   elseif(NOT CMAKE_BUILD_TYPE IN_LIST _allowed_build_types)
     message(FATAL_ERROR "Invalid build type: ${CMAKE_BUILD_TYPE}")
   endif()
