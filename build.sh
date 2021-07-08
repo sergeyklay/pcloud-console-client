b883b6caef7c7eff3c37662099890d84c294450f
Serghei Iakovlev <egrep@protonmail.ch>
2021-07-08 10:38:07 +0300

    Add install script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+pushd pCloudCC/lib/pclsync || exit 1
+  echo "Build pclsync"
+  make clean fs
+popd || exit
+
+pushd pCloudCC/lib/mbedtls || exit 1
+  echo "Configure Mbed TLS"
+  cmake -S . -B . -DCMAKE_C_STANDARD=99 -DCMAKE_BUILD_TYPE=Release
+
+  echo "Build Mbed TLS"
+  cmake --build . --config Release
+popd || exit
+
+pushd pCloudCC || exit 1
+  echo "Configure client"
+  cmake -S . -B .build -DCMAKE_BUILD_TYPE=Release
+
+  echo "Build client"
+  cmake --build .build --config Release
+
+  echo "Install client"
+  sudo make -C .build install
+  sudo ldconfig
+popd || exit
