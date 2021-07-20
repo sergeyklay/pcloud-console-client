2d0ae44fcb0a8415b092f58f3eb2735eb2a6ceda
Serghei Iakovlev <egrep@protonmail.ch>
2021-07-20 09:11:41 +0300

    Amend documentation

--- src/psync/pdeviceid.h
+++ src/psync/pdeviceid.h
@@ -16,14 +16,28 @@ extern "C" {
 
 char *psync_get_device_id();
 
-/*! \brief Set the name and version of the operating system that is passed to
- *         the server during token creation. If not set, it will be
+/*! \brief Set the name (and version) of the operating system that is passed
+ *         to the server during token creation. If not set, it will be
  *         automatically detected.
  */
 void psync_set_os_name(const char *name);
 
+/*! \brief Set the name (and version) of the software that is passed to the
+ *         server during token creation.
+ *
+ * This function is to be called before psync_start_sync() and it is acceptable
+ * to call it even before psync_init().
+ *
+ * \note Library will not make its own copy, so either pass a static string or
+ *       make a copy of your dynamic string.
+ */
 void psync_set_software_name(const char *name);
 
+/*! \brief Get the name (and version) of the software that is passed to the
+ *         server during token creation.
+ *
+ *  If not set by psync_set_software_name() will return PSYNC_VERSION_STRING.
+ */
 const char *psync_get_software_name();
 
 char *psync_get_device_os();
