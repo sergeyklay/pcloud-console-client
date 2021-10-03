529de0de4f8471adfe07defa0bdd26aaebb120c2
Serghei Iakovlev <egrep@protonmail.ch>
2021-10-03 12:42:23 +0300

    Rewrite FindMbedTLS Cmake module

--- cmake/FindMbedTLS.cmake
+++ cmake/FindMbedTLS.cmake
@@ -1,104 +1,144 @@
-# - Try to find mbedTLS
+# This file is part of the pCloud Console Client.
+#
+# (c) 2021 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# For the full copyright and license information, please view
+# the LICENSE file that was distributed with this source code.
+
+# Find the MbedTLS includes and library
+#
 # Once done this will define
 #
-#  MBEDTLS_FOUND - system has mbedTLS
-#  MBEDTLS_INCLUDE_DIRS - the mbedTLS include directory
-#  MBEDTLS_LIBRARIES - Link these to use mbedTLS
-#  MBEDTLS_DEFINITIONS - Compiler switches required for using mbedTLS
-#=============================================================================
-#  Copyright (c) 2017 Sartura d.o.o.
-#
-#  Author: Juraj Vijtiuk <juraj.vijtiuk@sartura.hr>
-#
-#  Distributed under the OSI-approved BSD License (the "License");
-#  see accompanying file Copyright.txt for details.
-#
-#  This software is distributed WITHOUT ANY WARRANTY; without even the
-#  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#  See the License for more information.
-#=============================================================================
-#
+#  MBEDTLS_FOUND        - True if MbedTLS is found.
+#  MBEDTLS_INCLUDE_DIRS - The MbedTLS include directory.
+#  MBEDTLS_LIBRARIES    - Link these to use MbedTLS.
 
+find_package(PkgConfig QUIET)
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules(_MBEDTLS QUIET mbedtls)
+endif()
 
-set(_MBEDTLS_ROOT_HINTS
-  $ENV{MBEDTLS_ROOT_DIR}
-  ${MBEDTLS_ROOT_DIR})
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+  set(_lib_suffix 64)
+else()
+  set(_lib_suffix 32)
+endif()
 
-set(_MBEDTLS_ROOT_PATHS
-  "$ENV{PROGRAMFILES}/libmbedtls")
+find_path(
+  MBEDTLS_INCLUDE_DIR
+  NAMES mbedtls/ssl.h
+  HINTS ENV MBEDTLS_PATH${_lib_suffix}
+        ENV MBEDTLS_PATH
+        ${MBEDTLS_PATH${_lib_suffix}}
+        ${MBEDTLS_PATH}
+  PATHS /usr/include
+        /usr/local/include
+        /opt/local/include
+        /opt/homebrew/include
+  PATH_SUFFIXES include)
 
-set(_MBEDTLS_ROOT_HINTS_AND_PATHS
-  HINTS ${_MBEDTLS_ROOT_HINTS}
-  PATHS ${_MBEDTLS_ROOT_PATHS})
+if(APPLE)
+  set(MBEDTLS_LIB_NAMES libmbedtls.dylib mbedtls)
+else()
+  set(MBEDTLS_LIB_NAMES libmbedtls mbedtls)
+endif()
 
+find_library(
+  MBEDTLS_LIB
+  NAMES ${MBEDTLS_LIB_NAMES}
+  HINTS ENV MBEDTLS_PATH${_lib_suffix}
+        ENV MBEDTLS_PATH
+        ${MBEDTLS_PATH${_lib_suffix}}
+        ${MBEDTLS_PATH}
+  PATHS /lib64
+        /lib
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib64
+        /usr/local/lib
+        /opt/local/lib
+        /opt/homebrew/lib)
 
-find_path(MBEDTLS_INCLUDE_DIR
-  NAMES
-  mbedtls/config.h
-  HINTS
-  ${_MBEDTLS_ROOT_HINTS_AND_PATHS}
-  PATH_SUFFIXES
-  include
-  )
+message(STATUS "Check for mbedtls: ${MBEDTLS_LIB}")
 
-find_library(MBEDTLS_SSL_LIBRARY
-  NAMES
-  mbedtls
-  HINTS
-  ${_MBEDTLS_ROOT_HINTS_AND_PATHS}
-  PATH_SUFFIXES
-  lib
+if(APPLE)
+  set(MBEDCRYPTO_LIB_NAMES libmbedcrypto.dylib mbedcrypto)
+else()
+  set(MBEDCRYPTO_LIB_NAMES libmbedcrypto mbedcrypto)
+endif()
 
-  )
+find_library(
+  MBEDCRYPTO_LIB
+  NAMES ${MBEDCRYPTO_LIB_NAMES}
+  HINTS ENV MBEDTLS_PATH${_lib_suffix}
+        ENV MBEDTLS_PATH
+        ${MBEDTLS_PATH${_lib_suffix}}
+        ${MBEDTLS_PATH}
+  PATHS /lib64
+        /lib
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib64
+        /usr/local/lib
+        /opt/local/lib
+        /opt/homebrew/lib)
 
-find_library(MBEDTLS_CRYPTO_LIBRARY
-  NAMES
-  mbedcrypto
-  HINTS
-  ${_MBEDTLS_ROOT_HINTS_AND_PATHS}
-  PATH_SUFFIXES
-  lib
-  )
+message(STATUS "Check for mbedcrypto: ${MBEDCRYPTO_LIB}")
 
-find_library(MBEDTLS_X509_LIBRARY
-  NAMES
-  mbedx509
-  HINTS
-  ${_MBEDTLS_ROOT_HINTS_AND_PATHS}
-  PATH_SUFFIXES
-  lib
-  )
+if(APPLE)
+  set(MBEDX509_LIB_NAMES libmbedx509.dylib mbedx509)
+else()
+  set(MBEDX509_LIB_NAMES libmbedx509 mbedx509)
+endif()
 
-set(MBEDTLS_LIBRARIES ${MBEDTLS_SSL_LIBRARY} ${MBEDTLS_CRYPTO_LIBRARY}
-  ${MBEDTLS_X509_LIBRARY})
+find_library(
+  MBEDX509_LIB
+  NAMES ${MBEDX509_LIB_NAMES}
+  HINTS ENV MBEDTLS_PATH${_lib_suffix}
+        ENV MBEDTLS_PATH
+        ${MBEDTLS_PATH${_lib_suffix}}
+        ${MBEDTLS_PATH}
+  PATHS /lib64
+        /lib
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib64
+        /usr/local/lib
+        /opt/local/lib
+        /opt/homebrew/lib)
 
-if (MBEDTLS_INCLUDE_DIR AND EXISTS "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h")
-  file(STRINGS "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h" _mbedtls_version_str REGEX
-    "^#[\t ]*define[\t ]+MBEDTLS_VERSION_STRING[\t ]+\"[0-9]+.[0-9]+.[0-9]+\"")
+message(STATUS "Check for mbedx509: ${MBEDX509_LIB}")
 
-  string(REGEX REPLACE "^.*MBEDTLS_VERSION_STRING.*([0-9]+.[0-9]+.[0-9]+).*"
-    "\\1" MBEDTLS_VERSION "${_mbedtls_version_str}")
-endif ()
+# Sometimes mbedtls is split between three libs, and sometimes it isn't.
+# If it isn't, let's check if the symbols we need are all in MBEDTLS_LIB.
+if(MBEDTLS_LIB AND NOT MBEDCRYPTO_LIB AND NOT MBEDX509_LIB)
+  set(CMAKE_REQUIRED_LIBRARIES ${MBEDTLS_LIB})
+  set(CMAKE_REQUIRED_INCLUDES ${MBEDTLS_INCLUDE_DIR})
 
+  check_symbol_exists(mbedtls_x509_crt_init "mbedtls/x509_crt.h" MBEDTLS_INCLUDES_X509)
+  check_symbol_exists(mbedtls_sha256_init "mbedtls/sha256.h" MBEDTLS_INCLUDES_CRYPTO)
+  unset(CMAKE_REQUIRED_INCLUDES)
+  unset(CMAKE_REQUIRED_LIBRARIES)
+endif()
+
+# If we find all three libraries, then go ahead.
+if(MBEDTLS_LIB AND MBEDCRYPTO_LIB AND MBEDX509_LIB)
+  set(MBEDTLS_INCLUDE_DIRS ${MBEDTLS_INCLUDE_DIR})
+  set(MBEDTLS_LIBRARIES ${MBEDTLS_LIB} ${MBEDCRYPTO_LIB} ${MBEDX509_LIB})
+elseif(MBEDTLS_LIB AND MBEDTLS_INCLUDES_CRYPTO AND MBEDTLS_INCLUDES_X509)
+  # Otherwise, if we find MBEDTLS_LIB, and it has both CRYPTO and x509
+  # within the single lib (i.e. a windows build environment), then also
+  # feel free to go ahead.
+  set(MBEDTLS_INCLUDE_DIRS ${MBEDTLS_INCLUDE_DIR})
+  set(MBEDTLS_LIBRARIES ${MBEDTLS_LIB})
+endif()
+
+# Now we've accounted for the 3-vs-1 library case:
 include(FindPackageHandleStandardArgs)
-if (MBEDTLS_VERSION)
-  find_package_handle_standard_args(MbedTLS
-    REQUIRED_VARS
-    MBEDTLS_INCLUDE_DIR
-    MBEDTLS_LIBRARIES
-    VERSION_VAR
-    MBEDTLS_VERSION
-    FAIL_MESSAGE
-    "Could NOT find mbedTLS, try to set the path to mbedTLS root folder
-            in the system variable MBEDTLS_ROOT_DIR"
-    )
-else (MBEDTLS_VERSION)
-  find_package_handle_standard_args(MbedTLS
-    "Could NOT find mbedTLS, try to set the path to mbedLS root folder in
-        the system variable MBEDTLS_ROOT_DIR"
-    MBEDTLS_INCLUDE_DIR
-    MBEDTLS_LIBRARIES)
-endif (MBEDTLS_VERSION)
+find_package_handle_standard_args(MbedTLS
+  "Could NOT find mbedTLS, try to set the path to MbedTLS root folder in the system variable MBEDTLS_ROOT_DIR"
+  MBEDTLS_INCLUDE_DIRS
+  MBEDTLS_LIBRARIES)
 
 # show the MBEDTLS_INCLUDE_DIRS and MBEDTLS_LIBRARIES variables only in the advanced view
-mark_as_advanced(MBEDTLS_INCLUDE_DIR MBEDTLS_LIBRARIES)
+mark_as_advanced(MBEDTLS_INCLUDE_DIR MBEDTLS_LIB MBEDCRYPTO_LIB MBEDX509_LIB)
